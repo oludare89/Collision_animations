b00e0585909d69b858f2fb1c2d7917115d107520
oludare89 <azizoyekan@hotmail.com>
2022-11-27 18:19:40 +0000

    Added window load event listener for existing code in script.js file

--- script.js
+++ script.js
@@ -1,71 +1,73 @@
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
-canvas.width = 500;
-canvas.height = 700;
-const explosions = [];
-let canvasPosition = canvas.getBoundingClientRect();
-let framespeed = 10;
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 700;
+    const explosions = [];
+    let canvasPosition = canvas.getBoundingClientRect();
+    let framespeed = 10;
 
-class Explosion {
-    constructor(x,y){
-        this.spriteWidth = 200;
-        this.spriteHeight = 179;
-        this.width = this.spriteWidth * 0.5;
-        this.height = this.spriteHeight * 0.5;
-        this.x = x;
-        this.y = y;
-        this.image = new Image();
-        this.image.src = 'images/boom.png';
-        this.frame = 0;
-        this.timer = 0;
-        this.angle = Math.random() * 6.2;
-        this.sound = new Audio();
-        this.sound.src = 'sounds/boom.wav';
-    }
-    update(){
-        if (this.frame === 0){
-            this.sound.play();
+    class Explosion {
+        constructor(x,y){
+            this.spriteWidth = 200;
+            this.spriteHeight = 179;
+            this.width = this.spriteWidth * 0.5;
+            this.height = this.spriteHeight * 0.5;
+            this.x = x;
+            this.y = y;
+            this.image = new Image();
+            this.image.src = 'images/boom.png';
+            this.frame = 0;
+            this.timer = 0;
+            this.angle = Math.random() * 6.2;
+            this.sound = new Audio();
+            this.sound.src = 'sounds/boom.wav';
         }
-        this.timer++;
-        if (this.timer % framespeed === 0){
-            this.frame++;
+        update(){
+            if (this.frame === 0){
+                this.sound.play();
+            }
+            this.timer++;
+            if (this.timer % framespeed === 0){
+                this.frame++;
+            }
         }
-    }
-    draw(){
-        ctx.save();
-        ctx.translate(this.x, this.y)
-        ctx.rotate(this.angle);
-        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, 0 - this.width/2, 0 - this.height/2, this.spriteWidth, this.spriteHeight);
-        ctx.restore();
-    }
-};
+        draw(){
+            ctx.save();
+            ctx.translate(this.x, this.y)
+            ctx.rotate(this.angle);
+            ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, 0 - this.width/2, 0 - this.height/2, this.spriteWidth, this.spriteHeight);
+            ctx.restore();
+        }
+    };
 
-window.addEventListener('click', function(e){
-    createAnimation(e);
+    window.addEventListener('click', function(e){
+        createAnimation(e);
+    });
+
+    /* window.addEventListener('mousemove', function(e){
+        createAnimation(e);
+    }); */
+
+    function createAnimation(e){
+        console.log(explosions);
+        let positionX = e.x - canvasPosition.left;
+        let positionY = e.y - canvasPosition.top;
+        explosions.push(new Explosion(positionX, positionY));
+    }
+
+    function animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < explosions.length; i++){
+            explosions[i].update;
+            explosions[i].draw;
+            if (explosions[i] > 5){
+                explosions.splice(i, 1);
+                i--;
+            }
+        }
+        requestAnimationFrame(animate);
+    };
+
+    animate();
 });
-
-/* window.addEventListener('mousemove', function(e){
-    createAnimation(e);
-}); */
-
-function createAnimation(e){
-    console.log(explosions);
-    let positionX = e.x - canvasPosition.left;
-    let positionY = e.y - canvasPosition.top;
-    explosions.push(new Explosion(positionX, positionY));
-}
-
-function animate(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < explosions.length; i++){
-        explosions[i].update;
-        explosions[i].draw;
-        if (explosions[i] > 5){
-            explosions.splice(i, 1);
-            i--;
-        }
-    }
-    requestAnimationFrame(animate);
-};
-
-animate();
